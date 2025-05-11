201866b1e36c599e7315a70c5fb16566d58f2345
Takumi Saito <takumi1469@gmail.com>
2025-05-11 18:34:31 +0200

    Commit before deployment

--- Taccolo/obj/Debug/net8.0/Taccolo.AssemblyInfo.cs
+++ Taccolo/obj/Debug/net8.0/Taccolo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6294a54e32a09a3f527329ed363b1e042f321b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55742fa43bc1a82af02349ec5fdecf6781cfdc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
