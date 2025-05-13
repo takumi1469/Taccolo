a420db621462cc56a429a9084d18a8fd29b731e3
Takumi Saito <takumi1469@gmail.com>
2025-05-13 16:25:31 +0200

    Put try-catch in Program.cs

--- Taccolo/Program.cs
+++ Taccolo/Program.cs
@@ -6,72 +6,80 @@ using Microsoft.EntityFrameworkCore;
 
 //Comment to enable pushing 2
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add settings about logging configuration
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Identity-related services
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+try
 {
-    options.User.RequireUniqueEmail = true; // Enforce unique email
-}).AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
+    var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.Configure<IdentityOptions>(options =>
-//{
-//    options.SignIn.RequireConfirmedEmail = true; //require email verification for registration
-//});
+    // Add settings about logging configuration
+    builder.Logging.ClearProviders();
+    builder.Logging.AddConsole();
+    builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-//Add EmailSender to DI
-builder.Services.AddTransient<IEmailSender, EmailSender>();
 
-//Add AzureTranslator to DI
-builder.Services.AddSingleton<AzureTranslator>();
+    // Add services to the container.
+    builder.Services.AddRazorPages();
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//Add Session State
-builder.Services.AddSession(options =>
-{
-    options.Cookie.HttpOnly = true;  // Ensures cookies are only accessible by the server
-    options.IdleTimeout = TimeSpan.FromMinutes(20); // Set session timeout (optional)
-    options.Cookie.IsEssential = true; // Makes the session cookie essential
-});
+    // Identity-related services
+    builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.User.RequireUniqueEmail = true; // Enforce unique email
+    }).AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
 
-// Configure Authentication middleware
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Account/Login";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-});
+    //builder.Services.Configure<IdentityOptions>(options =>
+    //{
+    //    options.SignIn.RequireConfirmedEmail = true; //require email verification for registration
+    //});
 
-var app = builder.Build();
+    //Add EmailSender to DI
+    builder.Services.AddTransient<IEmailSender, EmailSender>();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //Add AzureTranslator to DI
+    builder.Services.AddSingleton<AzureTranslator>();
+
+    //Add Session State
+    builder.Services.AddSession(options =>
+    {
+        options.Cookie.HttpOnly = true;  // Ensures cookies are only accessible by the server
+        options.IdleTimeout = TimeSpan.FromMinutes(20); // Set session timeout (optional)
+        options.Cookie.IsEssential = true; // Makes the session cookie essential
+    });
+
+    // Configure Authentication middleware
+    builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSession();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    // Add Authentication and Authorization to the pipeline
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseRouting();
+    app.MapRazorPages();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseSession();
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-// Add Authentication and Authorization to the pipeline
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseRouting();
-app.MapRazorPages();
-app.MapControllers();
-
-app.Run();
+catch(Exception ex)
+{
+    File.WriteAllText("startup-error.txt", ex.ToString());
+    throw;
+}
