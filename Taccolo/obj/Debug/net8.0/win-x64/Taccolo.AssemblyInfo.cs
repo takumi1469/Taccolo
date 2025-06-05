b455d38e32314780732d28a5639a1ac950c41178
Takumi Saito <takumi1469@gmail.com>
2025-06-05 14:49:32 +0200

    Added favicon

--- Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
+++ Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39949e7a9a5545ca634a3755c73a8f5692ca381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f2c7f1cc093158320e79867fa92d3d5d8c5a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
