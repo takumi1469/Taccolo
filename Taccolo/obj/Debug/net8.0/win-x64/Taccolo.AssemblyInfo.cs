75da929197223c904e2004fa40eb9850e1152de9
Takumi Saito <takumi1469@gmail.com>
2025-06-09 22:45:06 +0200

    Added footer

--- Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
+++ Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8d14efbc57b228abd30076de5bb44e485da0c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3b61f51d744eccd036e01dca7338fe4ae44365")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
