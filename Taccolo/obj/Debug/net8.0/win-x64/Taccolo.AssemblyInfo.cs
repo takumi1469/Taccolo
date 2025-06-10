4973573b60bbead3db5004681a4765a0cdf89777
Takumi Saito <takumi1469@gmail.com>
2025-06-10 17:00:21 +0200

    Fixed footer
    But the resolution isn't quite final. We'll see.

--- Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
+++ Taccolo/obj/Debug/net8.0/win-x64/Taccolo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3b61f51d744eccd036e01dca7338fe4ae44365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59f6765ac78d543af4b3684bedd3de1a5894537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taccolo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
